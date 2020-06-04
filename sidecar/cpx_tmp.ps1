c4b88a87daa034b8e77c3ef04c81c2725adc43db
andreaz86 <a.zanna@live.it>
2020-06-04 17:11:01 +0200

    add portainer

--- sidecar/cpx_tmp.ps1
+++ sidecar/cpx_tmp.ps1
@@ -53,35 +53,43 @@ Enable-NSFeature -name RESPONDER -Force
 write-host "Creating Servicegroup"
 New-NSLBServiceGroup -Name "SG_CHECKMK" -Protocol HTTP -Session $Session -ErrorAction Continue -State ENABLED 
 New-NSLBServiceGroup -Name "SG_KIBANA" -Protocol HTTP -Session $Session -ErrorAction Continue -State ENABLED
+New-NSLBServiceGroup -Name "SG_PORTAINER" -Protocol HTTP -Session $Session -ErrorAction Continue -State ENABLED
 
 write-host "Creating VirtualServer"
 New-NSLBVirtualServer -Name "VS_CHECKMK" -ServiceType HTTP -NonAddressable -Session $Session 
 New-NSLBVirtualServer -Name "VS_KIBANA" -ServiceType HTTP -NonAddressable -Session $Session 
+New-NSLBVirtualServer -Name "VS_PORTAINER" -ServiceType HTTP -NonAddressable -Session $Session 
 
 write-host "Creating Server:"
 $checkmk= invoke-restmethod -uri "http://consul:8500/v1/catalog/service/ctxmonplatform_checkmk-5000" -ErrorAction Continue
 $kibana= invoke-restmethod -uri "http://consul:8500/v1/catalog/service/ctxmonplatform_kibana" -ErrorAction Continue
+$portainer= invoke-restmethod -uri "http://consul:8500/v1/catalog/service/portainer" -ErrorAction Continue
+
 New-NSLBServer -Name CHECKMK -IPAddress $checkmk[0].ServiceAddress -Session $Session -ErrorAction Continue -State ENABLED
 write-host "Created server CheckMK with IP $checkmk[0].ServiceAddress"
 New-NSLBServer -Name KIBANA -IPAddress $kibana[0].ServiceAddress -Session $Session -ErrorAction Continue -State ENABLED
 write-host "Created server Kibana with IP $kibana[0].ServiceAddress"
+New-NSLBServer -Name PORTAINER -IPAddress $portainer[0].ServiceAddress -Session $Session -ErrorAction Continue -State ENABLED
 
 write-host "Adding server to ServiceGroup:"
 New-NSLBServiceGroupMember -Name SG_CHECKMK -ServerName CHECKMK -Session $Session -Port "5000" -ErrorAction SilentlyContinue
 New-NSLBServiceGroupMember -Name SG_KIBANA -ServerName KIBANA -Session $Session -Port 5601 -ErrorAction SilentlyContinue
+New-NSLBServiceGroupMember -Name SG_PORTAINER -ServerName PORTAINER -Session $Session -Port 9000 -ErrorAction SilentlyContinue
 
 Add-NSLBVirtualServerBinding -VirtualServerName VS_CHECKMK -ServiceGroupName SG_CHECKMK -Session $Session -ErrorAction Continue
 Add-NSLBVirtualServerBinding -VirtualServerName VS_KIBANA -ServiceGroupName SG_KIBANA -Session $Session -ErrorAction Continue
+Add-NSLBVirtualServerBinding -VirtualServerName VS_PORTAINER -ServiceGroupName SG_PORTAINER -Session $Session -ErrorAction Continue
 
 New-NSCSVirtualServer -Name CS_HTTP -Session $Session -IPAddress $localip[0].ServiceAddress -Port 80 -ServiceType HTTP -State ENABLED
 New-NSCSVirtualServer -Name CS_HTTPS -Session $Session -IPAddress $localip[0].ServiceAddress -Port 443 -ServiceType SSL -State ENABLED
 
 New-NSCSPolicy -Name CS_POL_CHECKMK -Rule 'HTTP.REQ.HOSTNAME.EQ("checkmk.lab.local")'
 New-NSCSPolicy -Name CS_POL_KIBANA -Rule 'HTTP.REQ.HOSTNAME.EQ("kibana.lab.local")'
-
+New-NSCSPolicy -Name CS_POL_PORTAINER -Rule 'HTTP.REQ.HOSTNAME.EQ("portainer.lab.local")'
 
 Add-NSCSVirtualServerPolicyBinding -Name CS_HTTPS -PolicyName CS_POL_CHECKMK -TargetLBVServer VS_CHECKMK -Priority 100
 Add-NSCSVirtualServerPolicyBinding -Name CS_HTTPS -PolicyName CS_POL_KIBANA -TargetLBVServer VS_KIBANA -Priority 110
+Add-NSCSVirtualServerPolicyBinding -Name CS_HTTPS -PolicyName CS_POL_PORTAINER -TargetLBVServer VS_PORTAINER -Priority 120
 
 New-NSResponderAction -name RES_ACT_CHECKMK -Type Redirect -target '"/cmk/check_mk"' -ResponseStatusCode 301
 New-NSResponderPolicy -Name RES_POL_CHECKMK -Rule 'HTTP.REQ.URL.EQ("/")' -Action RES_ACT_CHECKMK
@@ -93,6 +101,8 @@ New-NSResponderPolicy -Name CS_POL_HTTP2HTTPS -Rule true -Action CS_ACT_HTTP2HTT
 Add-NSLBVirtualServerResponderPolicyBinding -PolicyName RES_POL_CHECKMK -Priority 100 -VirtualServerName VS_CHECKMK -Bindpoint REQUEST
 Add-NSCSVirtualServerResponderPolicyBinding -PolicyName CS_POL_HTTP2HTTPS -Priority 100 -VirtualServerName CS_HTTP -Bindpoint REQUEST 
 
+Add-NSCertKeyPair -CertKeyName "LabRootCA" -CertPath /nsconfig/ssl/RootCA.cer
 $password=ConvertTo-SecureString "Aa123456" -AsPlainText -Force
 Add-NSCertKeyPair -CertKeyName "LabWildcard" -CertPath /nsconfig/ssl/lab.local.pfx -CertKeyFormat PFX -Password $password
+Add-NSSSLCertificateLink -CertKeyName LabWildcard -IntermediateCertKeyName LabRootCA
 Add-NSLBSSLVirtualServerCertificateBinding -VirtualServerName CS_HTTPS -Certificate LabWildcard
