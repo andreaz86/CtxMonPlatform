8f812482a4eeeb41c93fb359543d7a19af0b8870
andreaz86 <a.zanna@live.it>
2020-06-04 17:49:08 +0200

    sdf

--- sidecar/cpx_tmp.ps1
+++ sidecar/cpx_tmp.ps1
@@ -50,50 +50,57 @@ $script:Session =  Connect-Netscaler -Hostname $nsip_port -PassThru -Credential 
 Enable-NSFeature -name CS -Force
 Enable-NSFeature -name RESPONDER -Force
 
+#################   SERVICE GROUP   #########################
 write-host "Creating Servicegroup"
-New-NSLBServiceGroup -Name "SG_CHECKMK" -Protocol HTTP -Session $Session -ErrorAction Continue -State ENABLED 
+New-NSLBServiceGroup -Name "SG_CHECKMK" -Protocol HTTP -Session $Session -ErrorAction Continue -State ENABLED
 New-NSLBServiceGroup -Name "SG_KIBANA" -Protocol HTTP -Session $Session -ErrorAction Continue -State ENABLED
 New-NSLBServiceGroup -Name "SG_PORTAINER" -Protocol HTTP -Session $Session -ErrorAction Continue -State ENABLED
 
+#################   VIRTUAL SERVER ###########################
 write-host "Creating VirtualServer"
 New-NSLBVirtualServer -Name "VS_CHECKMK" -ServiceType HTTP -NonAddressable -Session $Session 
 New-NSLBVirtualServer -Name "VS_KIBANA" -ServiceType HTTP -NonAddressable -Session $Session 
 New-NSLBVirtualServer -Name "VS_PORTAINER" -ServiceType HTTP -NonAddressable -Session $Session 
 
+################    SERVER  ################################
 write-host "Creating Server:"
 $checkmk= invoke-restmethod -uri "http://consul:8500/v1/catalog/service/ctxmonplatform_checkmk-5000" -ErrorAction Continue
 $kibana= invoke-restmethod -uri "http://consul:8500/v1/catalog/service/ctxmonplatform_kibana" -ErrorAction Continue
 $portainer= invoke-restmethod -uri "http://consul:8500/v1/catalog/service/portainer" -ErrorAction Continue
-
 New-NSLBServer -Name CHECKMK -IPAddress $checkmk[0].ServiceAddress -Session $Session -ErrorAction Continue -State ENABLED
-write-host "Created server CheckMK with IP $checkmk[0].ServiceAddress"
+write-host "Created server CheckMK with IP" $checkmk[0].ServiceAddress
 New-NSLBServer -Name KIBANA -IPAddress $kibana[0].ServiceAddress -Session $Session -ErrorAction Continue -State ENABLED
-write-host "Created server Kibana with IP $kibana[0].ServiceAddress"
+write-host "Created server Kibana with IP" $kibana[0].ServiceAddress
 New-NSLBServer -Name PORTAINER -IPAddress $portainer[0].ServiceAddress -Session $Session -ErrorAction Continue -State ENABLED
 
+##############  ADD SERVER TO SERVICEGROUP  ########################
 write-host "Adding server to ServiceGroup:"
 New-NSLBServiceGroupMember -Name SG_CHECKMK -ServerName CHECKMK -Session $Session -Port "5000" -ErrorAction SilentlyContinue
 New-NSLBServiceGroupMember -Name SG_KIBANA -ServerName KIBANA -Session $Session -Port 5601 -ErrorAction SilentlyContinue
 New-NSLBServiceGroupMember -Name SG_PORTAINER -ServerName PORTAINER -Session $Session -Port 9000 -ErrorAction SilentlyContinue
 
+############### ADD SERVICEGROUP TO VSERVER #######################
 Add-NSLBVirtualServerBinding -VirtualServerName VS_CHECKMK -ServiceGroupName SG_CHECKMK -Session $Session -ErrorAction Continue
 Add-NSLBVirtualServerBinding -VirtualServerName VS_KIBANA -ServiceGroupName SG_KIBANA -Session $Session -ErrorAction Continue
 Add-NSLBVirtualServerBinding -VirtualServerName VS_PORTAINER -ServiceGroupName SG_PORTAINER -Session $Session -ErrorAction Continue
 
+############### CREATING CONTENT SWITCHING  #######################
 New-NSCSVirtualServer -Name CS_HTTP -Session $Session -IPAddress $localip[0].ServiceAddress -Port 80 -ServiceType HTTP -State ENABLED
 New-NSCSVirtualServer -Name CS_HTTPS -Session $Session -IPAddress $localip[0].ServiceAddress -Port 443 -ServiceType SSL -State ENABLED
 
+############### CREATING CS POLICY  ############################
 New-NSCSPolicy -Name CS_POL_CHECKMK -Rule 'HTTP.REQ.HOSTNAME.EQ("checkmk.lab.local")'
 New-NSCSPolicy -Name CS_POL_KIBANA -Rule 'HTTP.REQ.HOSTNAME.EQ("kibana.lab.local")'
 New-NSCSPolicy -Name CS_POL_PORTAINER -Rule 'HTTP.REQ.HOSTNAME.EQ("portainer.lab.local")'
 
+#############   ASSIGN CS POLICY TO CS VSERVER  ################
 Add-NSCSVirtualServerPolicyBinding -Name CS_HTTPS -PolicyName CS_POL_CHECKMK -TargetLBVServer VS_CHECKMK -Priority 100
 Add-NSCSVirtualServerPolicyBinding -Name CS_HTTPS -PolicyName CS_POL_KIBANA -TargetLBVServer VS_KIBANA -Priority 110
 Add-NSCSVirtualServerPolicyBinding -Name CS_HTTPS -PolicyName CS_POL_PORTAINER -TargetLBVServer VS_PORTAINER -Priority 120
 
+############# RESPONDER POLICY  ##############################
 New-NSResponderAction -name RES_ACT_CHECKMK -Type Redirect -target '"/cmk/check_mk"' -ResponseStatusCode 301
 New-NSResponderPolicy -Name RES_POL_CHECKMK -Rule 'HTTP.REQ.URL.EQ("/")' -Action RES_ACT_CHECKMK
-
 New-NSResponderAction -Name CS_ACT_HTTP2HTTPS -Type Redirect -target '"https://" + HTTP.REQ.HOSTNAME.HTTP_URL_SAFE + HTTP.REQ.URL.PATH_AND_QUERY.HTTP_URL_SAFE' -ResponseStatusCode 301
 New-NSResponderPolicy -Name CS_POL_HTTP2HTTPS -Rule true -Action CS_ACT_HTTP2HTTPS
 
